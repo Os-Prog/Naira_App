65f5833762095a932078a1ea1f088f9ee815061b
softcoder404 <daseeker9@gmail.com>
2020-03-07 10:45:34 +0100

    confirm page

---
+++ cash_app/lib/pages/confirm_payment.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ConfirmPaymentScreen extends StatefulWidget {
+  @override
+  _ConfirmPaymentScreenState createState() => _ConfirmPaymentScreenState();
+}
+
+class _ConfirmPaymentScreenState extends State<ConfirmPaymentScreen> {
+  Color _activeButtonColor = Colors.blue;
+  Color _inactiveButtonColor = Colors.black26;
+  bool _isbuttonActive = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Icon(FontAwesomeIcons.times),
+        ),
+        title: Text('Security Question'),
+        actions: <Widget>[
+          Container(
+              height: 20,
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              padding: EdgeInsets.only(right: 15, left: 10),
+              child: RaisedButton(
+                elevation: 10,
+                onPressed: () {
+                  if (_isbuttonActive)
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ConfirmPaymentScreen()));
+                },
+                color:
+                    _isbuttonActive ? _activeButtonColor : _inactiveButtonColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Container(
+                  child: Text(
+                    'Confirm',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )),
+        ],
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
+          child: Container(
+            color: Theme.of(context).primaryColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 220,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 0),
+                  child: Image.asset(
+                    'assets/images/security.png',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Container(
+                  color: Theme.of(context).primaryColor,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              'Security Questions',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 22),
+                            ),
+                            margin: EdgeInsets.only(bottom: 15),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              obscureText: true,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                              cursorColor: Colors.white,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.white)),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                ),
+                                labelText: 'Enter your transfer code',
+                                labelStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                                contentPadding:
+                                    EdgeInsets.fromLTRB(19, 5, 10, 5),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 27.0,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              obscureText: true,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                              cursorColor: Colors.white,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.white)),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                ),
+                                labelText: 'Enter your safe key',
+                                labelStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                                contentPadding:
+                                    EdgeInsets.fromLTRB(19, 5, 10, 5),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 27.0,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              obscureText: true,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                              cursorColor: Colors.white,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.white)),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                ),
+                                labelText: 'Enter number of transaction',
+                                labelStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                                contentPadding:
+                                    EdgeInsets.fromLTRB(19, 5, 10, 5),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- cash_app/lib/pages/paynow.dart
+++ cash_app/lib/pages/paynow.dart
@@ -1,5 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'confirm_payment.dart';
 
 class PayNow extends StatefulWidget {
   final paynowamount;
@@ -9,6 +12,16 @@ class PayNow extends StatefulWidget {
 }
 
 class _PayNowState extends State<PayNow> {
+  Color _activeButtonColor = Colors.blue;
+  Color _inactiveButtonColor = Colors.black26;
+  bool _isbuttonActive = false;
+  TextStyle _formTextStyle = TextStyle(fontSize: 20, letterSpacing: 1.5);
+
+  TextStyle _labelTextStyle = TextStyle(
+      color: Colors.black87,
+      fontWeight: FontWeight.w500,
+      fontSize: 20,
+      letterSpacing: 1.0);
   final payamount;
   String sendtoemail;
   String sendnote;
@@ -19,125 +32,156 @@ class _PayNowState extends State<PayNow> {
     return Scaffold(
       appBar: AppBar(
         leading: GestureDetector(
-          onTap: (){
+          onTap: () {
             Navigator.of(context).pop();
           },
           child: Icon(FontAwesomeIcons.times),
         ),
-        title: Text('NGN'+payamount.toString()),
-        centerTitle: true,
+        title: Text('₦' + payamount.toString()),
         actions: <Widget>[
           Container(
-            height: 20,
-            margin: EdgeInsets.symmetric(vertical: 10.0),
-            padding: EdgeInsets.only(right: 15, left: 10),
-            child: (isfieldperfect) ? RaisedButton(
-              onPressed: (){
-
-              },
-              color: Colors.transparent,
-              shape: RoundedRectangleBorder(
+              height: 20,
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              padding: EdgeInsets.only(right: 15, left: 10),
+              child: RaisedButton(
+                elevation: 10,
+                onPressed: () {
+                  if (_isbuttonActive)
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ConfirmPaymentScreen()));
+                },
+                color:
+                _isbuttonActive ? _activeButtonColor : _inactiveButtonColor,
+                shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30),
-              ),
-              child: Container(
-                child: Text('Pay', style: TextStyle(color: Colors.white),),
-              ),
-            ) : RaisedButton(
-              disabledColor: Colors.blueAccent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-                side: BorderSide(color: Colors.white12)
-              ),
-              child: Container(
-                  child: Text('Pay', style: TextStyle(color: Colors.white),),
-              ),
-            ),
-          ),
+                ),
+                child: Container(
+                  child: Text(
+                    'Pay',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )),
         ],
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Form(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 2),
-                  decoration: BoxDecoration(
-                      border: Border(bottom: BorderSide(width: 0.6, color: Colors.black45))
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(left: 10),
-                        child: Text('To:', style: TextStyle(fontWeight: FontWeight.w500),),
-                      ),
-                      Container(
-                        child: Expanded(
-                          child: Container(
-                            child: TextFormField(
-                              onChanged: (value){
-                                setState(() {
-                                  sendtoemail = value;
-//                                  isfieldperfect = ((sendtoemail.toString().isEmpty || sendtoemail == null) || (sendnote.toString().isEmpty || sendnote == null)) ? false : true;
-//                                  print(sendtoemail);
-//                                  print(sendnote);
-                                });
-                              },
-                              keyboardType: TextInputType.emailAddress,
-                              decoration: InputDecoration(
-                                hintText: 'Email, Phone number or Name',
-                                border: OutlineInputBorder(borderSide: BorderSide.none),
-                                contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 220,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/transfer.png'),
+                      fit: BoxFit.fill),
+                ),
+              ),
+              Form(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 60,
+                      margin: EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                  width: 0.6, color: Colors.black45))),
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              'To:',
+                              style: _labelTextStyle,
+                            ),
+                          ),
+                          Container(
+                            child: Expanded(
+                              child: Container(
+                                child: TextFormField(
+                                  style: _formTextStyle,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      sendtoemail = value;
+                                    });
+                                  },
+                                  keyboardType: TextInputType.number,
+                                  decoration: InputDecoration(
+                                    hintText: 'account number, userId',
+                                    border: OutlineInputBorder(
+                                        borderSide: BorderSide.none),
+                                    contentPadding:
+                                    EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 2),
-                  decoration: BoxDecoration(
-                      border: Border(bottom: BorderSide(width: 0.6, color: Colors.black45))
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(left: 10),
-                        child: Text('Note:', style: TextStyle(fontWeight: FontWeight.w500),),
-                      ),
-                      Container(
-                        child: Expanded(
-                          child: Container(
-                            child: TextFormField(
-                              onChanged: (value){
-                                sendnote = value;
-//                                isfieldperfect = ((sendtoemail.toString().isEmpty || sendtoemail.toString() == null) || (sendnote.toString().isEmpty || sendnote.toString() == null)) ? false : true;
-//                                print(sendtoemail.toString());
-//                                print(sendnote.toString());
-                              },
-                              keyboardType: TextInputType.text,
-                              decoration: InputDecoration(
-                                hintText: 'Add a short note description of transaction',
-                                border: OutlineInputBorder(borderSide: BorderSide.none),
-                                contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                  width: 0.6, color: Colors.black45))),
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              'Note:',
+                              style: _labelTextStyle,
+                            ),
+                          ),
+                          Container(
+                            child: Expanded(
+                              child: Container(
+                                child: TextFormField(
+                                  style: _formTextStyle,
+                                  onChanged: (value) {
+                                    sendnote = value;
+                                    if (value.length > 4) {
+                                      setState(() {
+                                        _isbuttonActive = true;
+                                      });
+                                    } else {
+                                      setState(() {
+                                        _isbuttonActive = false;
+                                      });
+                                    }
+                                  },
+                                  keyboardType: TextInputType.text,
+                                  decoration: InputDecoration(
+                                    hintText:
+                                    'Add a short note description of transaction',
+                                    border: OutlineInputBorder(
+                                        borderSide: BorderSide.none),
+                                    contentPadding:
+                                    EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
-  Future<void> isLoadingInd() async{
+
+  Future<void> isLoadingInd() async {
     return await showDialog<Null>(
         context: context,
         barrierDismissible: false,
@@ -147,7 +191,10 @@ class _PayNowState extends State<PayNow> {
             backgroundColor: Colors.transparent,
             children: <Widget>[
               Center(
-                child: CircularProgressIndicator(backgroundColor: Colors.deepOrange, strokeWidth: 1.3,),
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.deepOrange,
+                  strokeWidth: 1.3,
+                ),
               )
             ],
           );
